6b98562d902e706715920827b900ca325180c961
Petr Němec <xnemec93@vutbr.cz>
2021-03-05 14:34:04 +0100

    [LAB] 04-display

--- Labs/04-segment/display/display.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/obj/xsim_1.c
+++ Labs/04-segment/display/display.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/obj/xsim_1.c
@@ -54,13 +54,14 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-IKI_DLLESPEC extern void execute_9(char*, char *);
+IKI_DLLESPEC extern void execute_16(char*, char *);
 IKI_DLLESPEC extern void execute_10(char*, char *);
 IKI_DLLESPEC extern void execute_11(char*, char *);
 IKI_DLLESPEC extern void execute_12(char*, char *);
 IKI_DLLESPEC extern void execute_13(char*, char *);
 IKI_DLLESPEC extern void execute_14(char*, char *);
-IKI_DLLESPEC extern void execute_8(char*, char *);
+IKI_DLLESPEC extern void execute_15(char*, char *);
+IKI_DLLESPEC extern void execute_9(char*, char *);
 IKI_DLLESPEC extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
 IKI_DLLESPEC extern void transaction_2(char*, char*, unsigned, unsigned, unsigned);
 IKI_DLLESPEC extern void transaction_3(char*, char*, unsigned, unsigned, unsigned);
@@ -69,14 +70,14 @@ IKI_DLLESPEC extern void transaction_5(char*, char*, unsigned, unsigned, unsigne
 IKI_DLLESPEC extern void transaction_6(char*, char*, unsigned, unsigned, unsigned);
 IKI_DLLESPEC extern void transaction_7(char*, char*, unsigned, unsigned, unsigned);
 IKI_DLLESPEC extern void transaction_8(char*, char*, unsigned, unsigned, unsigned);
-funcp funcTab[15] = {(funcp)execute_9, (funcp)execute_10, (funcp)execute_11, (funcp)execute_12, (funcp)execute_13, (funcp)execute_14, (funcp)execute_8, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_2, (funcp)transaction_3, (funcp)transaction_4, (funcp)transaction_5, (funcp)transaction_6, (funcp)transaction_7, (funcp)transaction_8};
-const int NumRelocateId= 15;
+funcp funcTab[16] = {(funcp)execute_16, (funcp)execute_10, (funcp)execute_11, (funcp)execute_12, (funcp)execute_13, (funcp)execute_14, (funcp)execute_15, (funcp)execute_9, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_2, (funcp)transaction_3, (funcp)transaction_4, (funcp)transaction_5, (funcp)transaction_6, (funcp)transaction_7, (funcp)transaction_8};
+const int NumRelocateId= 16;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/top_behav/xsim.reloc",  (void **)funcTab, 15);
-	iki_vhdl_file_variable_register(dp + 4880);
-	iki_vhdl_file_variable_register(dp + 4936);
+	iki_relocate(dp, "xsim.dir/tb_top_behav/xsim.reloc",  (void **)funcTab, 16);
+	iki_vhdl_file_variable_register(dp + 4928);
+	iki_vhdl_file_variable_register(dp + 4984);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
@@ -84,12 +85,12 @@ void relocate(char *dp)
 
 void sensitize(char *dp)
 {
-	iki_sensitize(dp, "xsim.dir/top_behav/xsim.reloc");
+	iki_sensitize(dp, "xsim.dir/tb_top_behav/xsim.reloc");
 }
 
 void simulate(char *dp)
 {
-		iki_schedule_processes_at_time_zero(dp, "xsim.dir/top_behav/xsim.reloc");
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/tb_top_behav/xsim.reloc");
 	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
 	iki_execute_processes();
 
@@ -111,9 +112,9 @@ extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
 int main(int argc, char **argv)
 {
     iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
-    iki_set_sv_type_file_path_name("xsim.dir/top_behav/xsim.svtype");
-    iki_set_crvs_dump_file_path_name("xsim.dir/top_behav/xsim.crvsdump");
-    void* design_handle = iki_create_design("xsim.dir/top_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+    iki_set_sv_type_file_path_name("xsim.dir/tb_top_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/tb_top_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/tb_top_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
      iki_set_rc_trial_count(100);
     (void) design_handle;
     return iki_simulate_design();
