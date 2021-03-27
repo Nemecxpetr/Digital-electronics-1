bb8fff0037a3d2c4a2d035b540c3754a9f2afb5e
Petr Němec <xnemec93@vutbr.cz>
2021-03-27 18:14:02 +0100

    [LAB] Part 3 update

---
+++ Labs/07-ffs/ffs/ffs.srcs/sim_1/new/tb_t_ff_rst.vhd
@@ -0,0 +1,138 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 27.03.2021 17:39:06
+-- Design Name: 
+-- Module Name: tb_t_ff_rst - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_t_ff_rst is
+--  Port ( );
+end tb_t_ff_rst;
+
+architecture Behavioral of tb_t_ff_rst is
+
+  constant c_CLK_PERIOD : time    := 10 ns;
+
+  signal s_clk    : std_logic;
+  signal s_rst    : std_logic;
+  signal s_t      : std_logic;
+  signal s_q      : std_logic;
+  signal s_q_bar  : std_logic;
+
+begin
+    
+    uut_t_ff_rst : entity work.t_ff_rst
+    port map
+    (
+         clk   => s_clk,   
+         rst   => s_rst, 
+         t     => s_t,
+         q     => s_q,   
+         q_bar => s_q_bar
+    );
+    
+    --------------------------------------------------------------------
+    -- Clock generation process
+    --------------------------------------------------------------------
+    p_clk_gen : process
+    begin
+        while now < 750 ns loop         -- 75 periods of 100MHz clock
+            s_clk <= '0';
+            wait for c_CLK_PERIOD / 2;
+            s_clk <= '1';
+            wait for c_CLK_PERIOD / 2;
+        end loop;
+        wait;
+    end process p_clk_gen;
+    --------------------------------------------------------------------
+    -- Reset generation process
+    --------------------------------------------------------------------
+    p_reset_gen : process
+    begin
+        s_rst <= '0';
+        wait for 33 ns;
+        -- Reset activated
+        s_rst <= '1';
+        wait for 12 ns;
+        -- Reset deactivated
+        s_rst <= '0';
+        wait for 14 ns;
+        s_rst <= '1';
+        wait for 21 ns;
+        -- Reset deactivated
+        s_rst <= '0';
+        wait for 25 ns;
+        s_rst <= '1';
+        wait for 13 ns;
+        -- Reset deactivated
+        s_rst <= '0';
+        wait for 40 ns;
+        s_rst <= '1';
+        wait;
+    end process p_reset_gen;
+    --------------------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------------------
+    p_stimulus : process
+    begin
+        report "Stimulus process started" severity note;
+            s_q     <= '0';
+            s_q_bar <= '1';
+            s_t     <= '0';
+            wait for 20 ns;
+  
+            assert(s_q='0' and s_q_bar = '1')
+            report "huh" severity error;
+            
+            --d sekvence
+            s_t <= '1';
+            wait for 16 ns;
+            s_t <= '0';
+            wait for 16 ns;
+            s_t <= '1';
+            wait for 16 ns;
+            s_t <= '0';
+            wait for 16 ns;
+            s_t <= '1';
+            wait for 16 ns;
+            s_t <= '0';
+            wait for 16 ns;
+            s_t <= '1';
+            wait for 16 ns;
+            s_t <= '0';
+            wait for 16 ns;
+            
+            assert(s_q='0' and s_q_bar = '1')
+            report "huh" severity error;
+        
+        report "Stimulus process finished" severity note;
+        wait;
+    end process p_stimulus;
+
+end Behavioral;
--- Labs/07-ffs/ffs/ffs.srcs/sources_1/new/t_ff_rst.vhd
+++ Labs/07-ffs/ffs/ffs.srcs/sources_1/new/t_ff_rst.vhd
@@ -32,12 +32,37 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity t_ff_rst is
---  Port ( );
+    Port 
+(
+        clk    : in  STD_LOGIC;
+        rst    : in  STD_LOGIC;
+        t      : in  STD_LOGIC;
+        q      : out STD_LOGIC;
+        q_bar  : out STD_LOGIC
+);
 end t_ff_rst;
 
 architecture Behavioral of t_ff_rst is
+    signal s_q        : STD_LOGIC;
+    signal s_q_bar    : STD_LOGIC;
 
 begin
-
+    p_t_ff_rst : process (clk)
+    begin
+        if rising_edge(clk) then
+            if (rst = '1') then
+                q       <= '0';
+                q_bar   <= '1';
+            else
+                if (t = '0') then
+                    q       <= s_q;
+                    q_bar   <= s_q_bar;                
+                else
+                    q       <= not s_q;
+                    q_bar   <= not s_q_bar;
+                end if;
+            end if;
+        end if;
+    end process p_t_ff_rst;
 
 end Behavioral;
